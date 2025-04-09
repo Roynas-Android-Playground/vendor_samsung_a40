4a4254301e7593574db3e66da83e3da1a339b59a
royna2544 <whiteshell2544@naver.com>
2025-04-09 09:42:45 +0900

    a40: Commonize lib_SamsungRec_07010

--- a40-vendor.mk
+++ a40-vendor.mk
@@ -43,7 +43,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a40/proprietary/vendor/lib/hw/thermal.exynos7904.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/thermal.exynos7904.so \
     vendor/samsung/a40/proprietary/vendor/lib/libOpenCv.camera.samsung.so:$(TARGET_COPY_OUT_VENDOR)/lib/libOpenCv.camera.samsung.so \
     vendor/samsung/a40/proprietary/vendor/lib/libSEF.quram.so:$(TARGET_COPY_OUT_VENDOR)/lib/libSEF.quram.so \
-    vendor/samsung/a40/proprietary/vendor/lib/lib_SamsungRec_07010.so:$(TARGET_COPY_OUT_VENDOR)/lib/lib_SamsungRec_07010.so \
     vendor/samsung/a40/proprietary/vendor/lib/libexynoscamera3.so:$(TARGET_COPY_OUT_VENDOR)/lib/libexynoscamera3.so \
     vendor/samsung/a40/proprietary/vendor/lib/libsaiv_BeautySolutionVideo.so:$(TARGET_COPY_OUT_VENDOR)/lib/libsaiv_BeautySolutionVideo.so \
     vendor/samsung/a40/proprietary/vendor/lib/libuniapi.so:$(TARGET_COPY_OUT_VENDOR)/lib/libuniapi.so \
